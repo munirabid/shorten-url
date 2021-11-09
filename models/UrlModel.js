32feb701064b5685325f86930dbdb37b5a7b1601
Muneer Abid <munirabid50@gmail.com>
2021-11-09 21:46:47 +0100

    initial commit

---
+++ models/UrlModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// instantiate a mongoose schema
+const URLSchema = new mongoose.Schema({
+  urlCode: String,
+  longUrl: String,
+  shortUrl: String,
+  visitCount: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: String,
+    default: Date.now,
+  },
+});
+
+// create a model from schema and export it
+module.exports = mongoose.model("Url", URLSchema);
